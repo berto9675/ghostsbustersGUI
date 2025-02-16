e30732809c34a99b67782f04aca4b0b7fe606bf3
erikamc99 <erikamc99@gmail.com>
2025-02-16 23:31:35 +0100

    refactor UserModelTest

--- ghostbustersgui/src/test/java/dev/celia/ghostbustersgui/model/UserModelTest.java
+++ ghostbustersgui/src/test/java/dev/celia/ghostbustersgui/model/UserModelTest.java
@@ -3,17 +3,33 @@ package dev.celia.ghostbustersgui.model;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserModelTest {
 
     private UserModel userModel;
-    
+    private GhostModel ghost1;
+    private GhostModel ghost2;
+    private GhostModel ghost3;
+
+    @BeforeEach
+    void setUp() {
+        userModel = new UserModel();
+
+        ghost1 = new GhostModel("Pepito", GhostClass.CLASS_I, "Alto", "Invisibilidad", "15-03-2023");
+        ghost2 = new GhostModel("Menganito", GhostClass.CLASS_II, "Medio", "Teletransportación", "08-03-2023");
+        ghost3 = new GhostModel("Lucifer", GhostClass.CLASS_I, "Crítico", "Grito mortal", "22-05-2023");
+
+        userModel.addGhost(ghost1);
+        userModel.addGhost(ghost2);
+        userModel.addGhost(ghost3);
+    }
+
     @Test
     @DisplayName("Test para capturar un fantasma")
     void testAddGhost() {
-        userModel = new UserModel();
         GhostModel ghost = new GhostModel("Casper", GhostClass.CLASS_I, "Bajo", "Mimetización", "10-06-2023");
         userModel.addGhost(ghost);
         assertThat(userModel.getGhosts(), hasItem(ghost));
@@ -22,8 +38,7 @@ public class UserModelTest {
     @Test
     @DisplayName("Test para que al añadir un fantasma de null")
     void testAddNullGhost() {
-        userModel = new UserModel();
         userModel.addGhost(null);
-        assertThat(userModel.getGhosts().size(), is(0));
+        assertThat(userModel.getGhosts().size(), is(3));
     }
 }
