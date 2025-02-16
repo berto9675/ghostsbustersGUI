b655bc1864237200752fb59675985ca9e7e30bfe
erikamc99 <erikamc99@gmail.com>
2025-02-17 00:24:00 +0100

    test to open views created and passed

--- ghostbustersgui/src/test/java/dev/celia/ghostbustersgui/controller/UserControllerTest.java
+++ ghostbustersgui/src/test/java/dev/celia/ghostbustersgui/controller/UserControllerTest.java
@@ -3,26 +3,40 @@ package dev.celia.ghostbustersgui.controller;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
 
 import dev.celia.ghostbustersgui.model.GhostClass;
 import dev.celia.ghostbustersgui.model.GhostModel;
 import dev.celia.ghostbustersgui.model.UserModel;
+import dev.celia.ghostbustersgui.view.HomeScreen;
+import dev.celia.ghostbustersgui.view.MenuView;
 
 public class UserControllerTest {
-    private UserModel userModelMock;
+    @Mock
     private UserController userController;
+    
+    @Mock
+    private UserModel userModelMock;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userModelMock = mock(UserModel.class);
+        userController = new UserController(userModelMock);
+    }
 
     @Test
     @DisplayName("Test para capturar un fantasma")
     void testCaptureGhost() {
-        
-        userModelMock = mock(UserModel.class);
-        userController = new UserController(userModelMock);
 
         String name = "Casper";
         GhostClass ghostClass = GhostClass.CLASS_II;
@@ -44,5 +58,22 @@ public class UserControllerTest {
         assertThat(capturedGhost.getDangerLvl(), is(dangerLevel));
         assertThat(capturedGhost.getAbility(), is(ability));
         assertThat(capturedGhost.getCaptureDate(), is(captureDate));
+    }
+
+    @Test
+    @DisplayName("Test para verificar que se abre HomeScreen")
+    void testOpenHomeScreen() {
+        assertDoesNotThrow(() -> new HomeScreen());
+    }
+
+    @Test
+    @DisplayName("Test para verificar que se abre MenuView")
+    void testOpenMenuView() {
+        assertDoesNotThrow(() -> new MenuView(userController));
+    }
+    @Test
+    @DisplayName("Test para verificar que se abre CreateGhostView")
+    void testOpenCreateGhostView() {
+        assertDoesNotThrow(() -> userController.openCreateGhostView());
     }
 }
